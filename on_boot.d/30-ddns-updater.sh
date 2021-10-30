c810bd3498145bb210bc6102e451361eb841c851
Pedro Pombeiro <noreply@pedro.pombei.ro>
2021-10-30 14:00:20 +0200

    Replace non-working ddclient with ddns-updater container

---
+++ on_boot.d/30-ddns-updater.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+CONTAINER=ddns-updater
+
+# Starts a ddns-updater container that is deleted after it is stopped.
+# Config stored in /mnt/data/etc-ddns-updater/config.json
+if podman container exists "$CONTAINER"; then
+  podman start "$CONTAINER"
+else
+  podman run -i -d --rm \
+    --name "$CONTAINER" \
+    --net="host" \
+    --security-opt=no-new-privileges \
+    -e LISTENING_PORT=8001 \
+    -v /mnt/data/etc-ddns-updater:/updater/data \
+    docker.io/qmcgaw/ddns-updater
+fi
