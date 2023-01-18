b3c0576b876d6cb6ec9bba9395016076eda62887
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-01-18 22:17:02 +0100

    pihole: Update to 2023.01.2

--- scripts/upd_pihole.sh
+++ scripts/upd_pihole.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DOCKER_TAG=2023.01
+DOCKER_TAG=2023.01.2
 
 chmod +r /mnt/data/etc-pihole/* /mnt/data/pihole/* /mnt/data/pihole/etc-dnsmasq.d/*
 chmod 0664 /mnt/data/etc-pihole/gravity.db
--- scripts/upd_pihole_dote.sh
+++ scripts/upd_pihole_dote.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-DOCKER_TAG=2023.01
+DOCKER_TAG=2023.01.2
 tmpdir="$(mktemp -d)"
 curl -sSLo "${tmpdir}/dote" https://github.com/chrisstaite/DoTe/releases/latest/download/dote_arm64
 
