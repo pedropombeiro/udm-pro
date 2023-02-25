3cf50f40c460373986141a818503bad4b44029a4
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-02-25 21:20:31 +0100

    Simplify 06-cni-bridge.sh

--- on_boot.d/06-cni-bridge.sh
+++ on_boot.d/06-cni-bridge.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 ## Create network bridge for CNI
-
-ip link show cni0 > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if ! ip link show cni0 > /dev/null 2>&1; then
     ip link add cni0 type bridge
 fi
