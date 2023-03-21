95d4f2cf47e49645c24b48b7932cd97fa8dfe45e
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-21 23:03:18 +0100

    unbound: Use fully recursive resolution

--- on_boot.d/11-unbound.sh
+++ on_boot.d/11-unbound.sh
@@ -12,6 +12,11 @@ if [[ ! -f ${DATA_DIR}/unbound/etc/unbound_server.pem ]]; then
     alpinelinux/unbound:latest
 fi
 
+if [[ ! -s ${DATA_DIR}/unbound/lib/root.hints ]]; then
+  mkdir -p "${DATA_DIR}/unbound/lib"
+  wget https://www.internic.net/domain/named.root -q -O "${DATA_DIR}/unbound/lib/root.hints"
+fi
+
 if podman container exists "${CONTAINER}"; then
   podman start "${CONTAINER}"
 else
@@ -19,6 +24,7 @@ else
     --name "${CONTAINER}" \
     -e TZ="$(cat /data/system/timezone)" \
     -v "${DATA_DIR}/unbound/etc/:/etc/unbound/" \
+    -v "${DATA_DIR}/unbound/lib/root.hints:/var/lib/unbound/root.hints:ro" \
     -v "${DATA_DIR}/unbound/backup/:/var/tmp/unbound/" \
     alpinelinux/unbound:latest
 fi
