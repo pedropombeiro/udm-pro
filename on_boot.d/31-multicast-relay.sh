8f94599b0664ec582cb14ced4fc1bdfc2dd077ef
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-03-21 23:12:55 +0100

    Disable multicast relay

--- on_boot.d/31-multicast-relay.sh
+++ on_boot.d/31-multicast-relay.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-CONTAINER=multicast-relay
-
-# kill all instances of avahi-daemon (UDM spins an instance up even with mDNS services disabled)
-killall avahi-daemon
-
-# Starts a multicast-relay container that is deleted after it is stopped.
-if podman container exists "${CONTAINER}"; then
-  podman start "${CONTAINER}"
-else
-  podman run -d \
-    --name "${CONTAINER}" \
-    --net="host" \
-    --security-opt=no-new-privileges \
-    --restart=always \
-    -e OPTS="--noSonosDiscovery" \
-    -e INTERFACES="eth8 br46 br56 br76 br96" \
-    -e TZ="$(cat /data/system/timezone)" \
-    scyto/multicast-relay
-fi
+# CONTAINER=multicast-relay
+#
+# # kill all instances of avahi-daemon (UDM spins an instance up even with mDNS services disabled)
+# killall avahi-daemon
+#
+# # Starts a multicast-relay container that is deleted after it is stopped.
+# if podman container exists "${CONTAINER}"; then
+#   podman start "${CONTAINER}"
+# else
+#   podman run -d \
+#     --name "${CONTAINER}" \
+#     --net="host" \
+#     --security-opt=no-new-privileges \
+#     --restart=always \
+#     -e OPTS="--noSonosDiscovery" \
+#     -e INTERFACES="eth8 br46 br56 br76 br96" \
+#     -e TZ="$(cat /data/system/timezone)" \
+#     scyto/multicast-relay
+# fi
