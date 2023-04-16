776de86c331cb67f6a3deb6e98b37403d667e79f
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-04-16 16:56:21 +0200

    multicast-relay: Fix permissions for UniFi OS 2.5.x

--- on_boot.d/31-multicast-relay.sh
+++ on_boot.d/31-multicast-relay.sh
@@ -2,6 +2,8 @@
 
 CONTAINER=multicast-relay
 
+# This container fixes situations like casting from a laptop to the Android TV box
+
 # kill all instances of avahi-daemon (UDM spins an instance up even with mDNS services disabled)
 killall avahi-daemon
 
@@ -12,7 +14,7 @@ else
   podman run -d \
     --name "${CONTAINER}" \
     --net="host" \
-    --security-opt=no-new-privileges \
+    --privileged \
     --restart=always \
     -e OPTS="--noSonosDiscovery" \
     -e INTERFACES="eth8 br46 br56 br76 br96" \
