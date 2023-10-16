0e02cd33590c4f905307ea93d310193099b9d905
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-10-17 00:57:17 +0200

    unbound: Restore from msg entries

--- scripts/backup_unbound_cache.sh
+++ scripts/backup_unbound_cache.sh
@@ -11,9 +11,9 @@ set +e
 if machinectl show "${CONTAINER}" | grep 'State=running'; then
   echo 'Saving Unbound cache...'
   machinectl shell "${CONTAINER}" /usr/sbin/unbound-control dump_cache > "${CACHE_DUMP_PATH}"
-  domains=$(grep -E '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' < "${CACHE_DUMP_PATH}" | \
-    sed --regexp-extended 's/([a-z0-9\-\.]+)\.\s+.*/\1/' | \
-    sort | uniq | wc -l)
+  domains=$( grep --extended-regexp '^msg ([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\. IN (A|AAAA|CNAME)' < "${CACHE_DUMP_PATH}" | \
+    sed --regexp-extended 's/^msg (.*)\..+IN\s.*/\1/' | \
+    wc -l)
 
   [[ -s "$CACHE_DUMP_PATH" ]] && echo "Cache saved successfully (${domains} domains)" || echo 'Failed to save cache'
 fi
--- scripts/restore_unbound_cache.sh
+++ scripts/restore_unbound_cache.sh
@@ -7,12 +7,12 @@ source ${DATA_DIR}/scripts/.export_unbound_cache_env.sh
 if [[ -s ${CACHE_DUMP_PATH} ]]; then
   echo 'Restoring Unbound cache...'
   tmpfile="$(mktemp)"
-  grep -E '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+' < "${CACHE_DUMP_PATH}" | \
-    sed --regexp-extended 's/([a-z0-9\-\.]+)\.\s+.*/\1/' | \
-    sort | uniq > "$tmpfile"
+  grep --extended-regexp '^msg ([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\. IN (A|AAAA|CNAME)' < "${CACHE_DUMP_PATH}" | \
+    sed --regexp-extended 's/^msg (.*)\..+IN\s.*/\1/' \
+    > "$tmpfile"
   xargs --no-run-if-empty --max-procs=0 --max-args=16 -I _ sh -c "echo _: && dig +short -p 5335 @192.168.6.254 _ | sed 's/^/  /'" < "$tmpfile"
 
-  echo "Restored $(wc -l < "$tmpfile") domains from backup."
+  echo "Restored $(sort < "$tmpfile" | sort | uniq) domains from backup."
 
   rm -f "$tmpfile"
 fi
