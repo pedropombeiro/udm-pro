2249c9b57b8596a668fe0851459d89d6e02627c5
Pedro Pombeiro <noreply@pedro.pombei.ro>
2023-10-27 21:25:22 +0200

    unbound: Perform backup before replacing file

--- scripts/backup_unbound_cache.sh
+++ scripts/backup_unbound_cache.sh
@@ -10,9 +10,11 @@ set +e
 
 if machinectl show "${CONTAINER}" | grep 'State=running'; then
   echo 'Saving Unbound cache...'
-  machinectl shell "${CONTAINER}" /usr/sbin/unbound-control dump_cache > "${CACHE_DUMP_PATH}"
-  domains=$( grep --extended-regexp '^msg ([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\. IN (A|AAAA|CNAME)' < "${CACHE_DUMP_PATH}" | \
-    sed --regexp-extended 's/^msg (.*)\..+IN\s.*/\1/' | \
+  [[ -f "${CACHE_DUMP_PATH}" ]] && cp -f "${CACHE_DUMP_PATH}" "${CACHE_DUMP_PATH}.bak"
+
+  machinectl shell "${CONTAINER}" /usr/sbin/unbound-control dump_cache >"${CACHE_DUMP_PATH}"
+  domains=$(grep --extended-regexp '^msg ([a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,}\. IN (A|AAAA|CNAME)' <"${CACHE_DUMP_PATH}" |
+    sed --regexp-extended 's/^msg (.*)\..+IN\s.*/\1/' |
     wc -l)
 
   [[ -s "$CACHE_DUMP_PATH" ]] && echo "Cache saved successfully (${domains} domains)" || echo 'Failed to save cache'
